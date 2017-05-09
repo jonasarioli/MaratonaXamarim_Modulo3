7556ac292b92997977dc1d3d52703eed0d5310cc
Jonas Arioli <jarioli@lenovo.com>
2017-05-09 16:56:48 -0300

    Add project files.

---
+++ MaratonaXamarim_Modulo3/MaratonaXamarim_Modulo3/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MaratonaXamarim_Modulo3.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MaratonaXamarim_Modulo3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.btnCarregar.Clicked += async (sender, e) =>
+            {
+                var tags = await Api.GetTagsAsync();
+                this.lvwTags.ItemsSource = tags;
+            };
+        }
+    }
+}
